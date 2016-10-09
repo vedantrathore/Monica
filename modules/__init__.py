6741e39a34a0244eb851defae30d8252079d6bdc
Vedant Rathore <vedantr1998@gmail.com>
2016-10-09 20:35:30 +0530

    Bug fixes

--- modules/__init__.py
+++ modules/__init__.py
@@ -19,16 +19,23 @@ def process_query(input):
         pprint(result)
         action = result['action']
         parameters = result['parameters']
-        actionIncomplete = result['actionIncomplete']
-        if actionIncomplete is True:
-            # process_query(input)
-            return None, result['fulfillment']['speech']
+        if 'actionIncomplete' in result:
+            actionIncomplete = result['actionIncomplete']
+            if actionIncomplete is True:
+                # print action
+                # pprint(result['fulfillment']['speech'])
+                return None, result['fulfillment']['speech']
         if action in src.__all__:
+            # pprint(action)
+            # pprint(parameters)
             return action, parameters
         else:
+            # pprint(action)
+            # pprint(result['fulfillment']['speech'])
             return action, result['fulfillment']['speech']
     except:
-        return None, {}
+        pass
+        # return None, {}
 
 def search(input):
     action,parameters = process_query(input)
@@ -42,8 +49,10 @@ def search(input):
             else:
                 return TextTemplate('Something didn\'t work as expected! I\'ll report this to my authority.').get_message()
     elif action is not None:
+        print parameters
         return TextTemplate(str(parameters)).get_message()
     elif action is None and parameters is not None:
+        print TextTemplate(str(parameters)).get_message()
         return TextTemplate(str(parameters)).get_message()
     else:
         return TextTemplate('I\'m sorry; I\'m not sure I understand what you\'re trying to say .\nTry typing "help" or "request"').get_message()
